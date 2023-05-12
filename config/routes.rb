1ed406e27eae874ca717757beb33a9db70a48e95
Adam Cox <adam.cox@simplybusiness.co.uk>
2023-05-12 14:57:45 +0100

    fix typo in routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :jobs, only[:index, :new, :create]
+  resources :jobs, only: [:index, :new, :create]
   # Defines the root path route ("/")
   root "jobs#index"
 end
