96461f4ad0006030d961d4398cf3e9aae89cb3e6
Adam Cox <adam.cox@simplybusiness.co.uk>
2023-05-12 14:42:41 +0100

    add new job form

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -3,4 +3,22 @@ class JobsController < ApplicationController
   def index
     @jobs = Job.all
   end
+
+  def new
+    @job = Job.new
+  end
+
+  def create
+    @job = Job.new(job_params)
+    if @job.save
+      redirect_to action: :index
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+end
+
+private_methods
+def job_params
+  params.require(:job).permit(:title,:company,:location,:link)
 end
