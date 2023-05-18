7c2d555ea7924c81fbe2d6733932652c5f71f495
Adam Cox <adam.cox@simplybusiness.co.uk>
2023-05-18 10:47:26 +0100

    adds authentication for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :jobs, only: [:index, :new, :create, :edit, :update, :destroy]
   # Defines the root path route ("/")
