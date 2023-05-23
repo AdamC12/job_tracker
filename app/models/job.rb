9e08b446988a80ec7d3326bba5b058c52d0f800c
Adam Cox <adam.cox@simplybusiness.co.uk>
2023-05-23 14:49:34 +0100

    add created_by to Job model

--- app/models/job.rb
+++ app/models/job.rb
@@ -4,6 +4,7 @@ class Job < ApplicationRecord
   validates :company, presence:true
   validates :link, presence:true
   validates :status, presence:true, inclusion: { in: %w[Interested Applied Interviewed Offered Rejected] }
+  validates :created_by, presence:true
 
   def has_status?(job_status)
     status == job_status
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_17_174323) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_23_132808) do
   create_table "jobs", force: :cascade do |t|
     t.string "title"
     t.string "location"
@@ -19,6 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_17_174323) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status"
+    t.integer "created_by"
   end
 
   create_table "users", force: :cascade do |t|
