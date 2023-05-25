c89fc580861ab20698ccea9505959b2199bfdac9
Adam Cox <adam.cox@simplybusiness.co.uk>
2023-05-25 11:01:01 +0100

    refactor status list to get from Job model

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,8 +2,7 @@ class JobsController < ApplicationController
   before_action :set_job, only: [:update, :edit, :destroy]
   before_action :authenticate_user!, only: [:edit, :rejected_jobs]
   before_action :confirm_job_owner_is_current_user, only: [:edit, :destroy]
-  before_action :setup_job_status_list
-
+  before_action :set_job_status_list, only: [:new, :edit, :index, :rejected_jobs]
   def index
     @jobs = current_user&.jobs || []
   end
@@ -58,8 +57,7 @@ class JobsController < ApplicationController
     redirect_to root_path unless @job.user_id == current_user.id
   end
 
-  def setup_job_status_list
-    @status_list = %w[Interested Applied Interviewed Offered Rejected]
+  def set_job_status_list
+    @status_list = Job.status_list
   end
-
 end
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,13 +1,19 @@
 class Job < ApplicationRecord
+
+  STATUSES = %w[Interested Applied Interviewed Offered Rejected]
   validates :title, presence:true
   validates :location, presence:true
   validates :company, presence:true
   validates :link, presence:true
-  validates :status, presence:true, inclusion: { in: %w[Interested Applied Interviewed Offered Rejected] }
+  validates :status, presence:true, inclusion: { in: STATUSES }
 
   belongs_to :user
 
   def has_status?(job_status)
     status == job_status
+  end
+
+  def self.status_list
+    STATUSES
   end
 end
